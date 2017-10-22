e6f68b80869e34510d85d095f246ce4e27eb0919
DESKTOP-O5N9A7L\timur <timur.yigit@locumilabs.com>
2017-10-22 17:48:27 +0200

    - clean up project file

--- Projects/tigit/main.c
+++ Projects/tigit/main.c
@@ -30,8 +30,8 @@
 #error "Board is not equipped with enough amount of LEDs"
 #endif
 
-#define TASK_DELAY        200           /**< Task delay. Delays a LED0 task for 200 ms */
-#define TIMER_PERIOD      400          /**< Timer period. LED1 timer will expire after 1000 ms */
+#define TASK_DELAY        800           /**< Task delay. Delays a LED0 task for 200 ms */
+#define TIMER_PERIOD      500          /**< Timer period. LED1 timer will expire after 1000 ms */
 
 TaskHandle_t  led_toggle_task_handle;   /**< Reference to LED0 toggling FreeRTOS task. */
 TimerHandle_t led_toggle_timer_handle;  /**< Reference to LED1 toggling FreeRTOS timer. */
