dbffc92f189dd85e3db58ff4425754b2f64b1185
TimurLOCUMI <timur.yigit@locumilabs.com>
2017-10-29 01:19:17 +0200

    - added cb templates

--- Projects/tigit/main.c
+++ Projects/tigit/main.c
@@ -32,6 +32,9 @@
 
 #include "nrf_drv_rtc.h"
 
+#include "m2m_wifi.h"
+#include "socket.h"
+
 #if LEDS_NUMBER <= 2
 #error "Board is not equipped with enough amount of LEDs"
 #endif
@@ -235,6 +238,186 @@ static void rtc_task_function (void * pvParameter)
         /* Delay a task for a given number of ticks */
         UNUSED_RETURN_VALUE(xSemaphoreTake(m_led_semaphore, portMAX_DELAY));
     }
+}
+
+/** Wi-Fi Settings */
+#define MAIN_WLAN_SSID         "DEMO_AP"
+#define MAIN_WLAN_AUTH          M2M_WIFI_SEC_WPA_PSK
+#define MAIN_WLAN_PSK          "12345678"
+
+/** Using NTP server information */
+#define MAIN_WORLDWIDE_NTP_POOL_HOSTNAME        "pool.ntp.org"
+#define MAIN_ASIA_NTP_POOL_HOSTNAME             "asia.pool.ntp.org"
+#define MAIN_EUROPE_NTP_POOL_HOSTNAME           "europe.pool.ntp.org"
+#define MAIN_NAMERICA_NTP_POOL_HOSTNAME         "north-america.pool.ntp.org"
+#define MAIN_OCEANIA_NTP_POOL_HOSTNAME          "oceania.pool.ntp.org"
+#define MAIN_SAMERICA_NTP_POOL_HOSTNAME         "south-america.pool.ntp.org"
+#define MAIN_SERVER_PORT_FOR_UDP                (123)
+#define MAIN_DEFAULT_ADDRESS                    0xFFFFFFFF /* "255.255.255.255" */
+#define MAIN_DEFAULT_PORT                       (6666)
+#define MAIN_WIFI_M2M_BUFFER_SIZE               1460
+
+/** Wi-Fi status variable. */
+static bool gbConnectedWifi = false;
+/** Host name placeholder. */
+static char dns_server_address[HOSTNAME_MAX_SIZE];
+
+/** UDP socket handlers. */
+static SOCKET udp_socket = -1;
+
+/** Receive buffer definition. */
+static uint8_t gau8SocketBuffer[MAIN_WIFI_M2M_BUFFER_SIZE];
+
+/**
+ * \brief Callback to get the Wi-Fi status update.
+ *
+ * \param[in] u8MsgType Type of Wi-Fi notification.
+ * \param[in] pvMsg A pointer to a buffer containing the notification parameters.
+ */
+static void wifi_cb(uint8_t u8MsgType, void *pvMsg)
+{
+	switch (u8MsgType) {
+	case M2M_WIFI_RESP_CON_STATE_CHANGED:
+	{
+		tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
+		if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
+			printf("wifi_cb: M2M_WIFI_RESP_CON_STATE_CHANGED: CONNECTED\r\n");
+			m2m_wifi_request_dhcp_client();
+		} else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
+			printf("wifi_cb: M2M_WIFI_RESP_CON_STATE_CHANGED: DISCONNECTED\r\n");
+			gbConnectedWifi = false;
+			m2m_wifi_connect((char *)MAIN_WLAN_SSID, sizeof(MAIN_WLAN_SSID),
+					MAIN_WLAN_AUTH, (char *)MAIN_WLAN_PSK, M2M_WIFI_CH_ALL);
+		}
+
+		break;
+	}
+
+	case M2M_WIFI_REQ_DHCP_CONF:
+	{
+		uint8_t *pu8IPAddress = (uint8_t *)pvMsg;
+		/* Turn LED0 on to declare that IP address received. */
+		printf("wifi_cb: M2M_WIFI_REQ_DHCP_CONF: IP is %u.%u.%u.%u\r\n",
+				pu8IPAddress[0], pu8IPAddress[1], pu8IPAddress[2], pu8IPAddress[3]);
+		gbConnectedWifi = true;
+		memcpy(dns_server_address, (uint8_t *)MAIN_WORLDWIDE_NTP_POOL_HOSTNAME, strlen(MAIN_WORLDWIDE_NTP_POOL_HOSTNAME));
+		/* Obtain the IP Address by network name */
+		gethostbyname((uint8_t *)dns_server_address);
+		break;
+	}
+
+	default:
+	{
+		break;
+	}
+	}
+}
+
+/**
+ * \brief Callback to get the ServerIP from DNS lookup.
+ *
+ * \param[in] pu8DomainName Domain name.
+ * \param[in] u32ServerIP Server IP.
+ */
+static void resolve_cb(uint8_t *pu8DomainName, uint32_t u32ServerIP)
+{
+	struct sockaddr_in addr;
+	int8_t cDataBuf[48];
+	int16_t ret;
+
+	memset(cDataBuf, 0, sizeof(cDataBuf));
+	cDataBuf[0] = '\x1b'; /* time query */
+
+	printf("resolve_cb: DomainName %s\r\n", pu8DomainName);
+
+	if (udp_socket >= 0) {
+		/* Set NTP server socket address structure. */
+		addr.sin_family = AF_INET;
+		addr.sin_port = _htons(MAIN_SERVER_PORT_FOR_UDP);
+		addr.sin_addr.s_addr = u32ServerIP;
+
+		/*Send an NTP time query to the NTP server*/
+		ret = sendto(udp_socket, (int8_t *)&cDataBuf, sizeof(cDataBuf), 0, (struct sockaddr *)&addr, sizeof(addr));
+		if (ret != M2M_SUCCESS) {
+			printf("resolve_cb: failed to send  error!\r\n");
+			return;
+		}
+	}
+}
+
+/**
+ * \brief Callback to get the Data from socket.
+ *
+ * \param[in] sock socket handler.
+ * \param[in] u8Msg Type of Socket notification.
+ * \param[in] pvMsg A structure contains notification informations.
+ */
+static void socket_cb(SOCKET sock, uint8_t u8Msg, void *pvMsg)
+{
+	/* Check for socket event on socket. */
+	int16_t ret;
+
+	switch (u8Msg) {
+	case SOCKET_MSG_BIND:
+	{
+		/* printf("socket_cb: socket_msg_bind!\r\n"); */
+		tstrSocketBindMsg *pstrBind = (tstrSocketBindMsg *)pvMsg;
+		if (pstrBind && pstrBind->status == 0) {
+			ret = recvfrom(sock, gau8SocketBuffer, MAIN_WIFI_M2M_BUFFER_SIZE, 0);
+			if (ret != SOCK_ERR_NO_ERROR) {
+				printf("socket_cb: recv error!\r\n");
+			}
+		} else {
+			printf("socket_cb: bind error!\r\n");
+		}
+
+		break;
+	}
+
+	case SOCKET_MSG_RECVFROM:
+	{
+		/* printf("socket_cb: socket_msg_recvfrom!\r\n"); */
+		tstrSocketRecvMsg *pstrRx = (tstrSocketRecvMsg *)pvMsg;
+		if (pstrRx->pu8Buffer && pstrRx->s16BufferSize) {
+			uint8_t packetBuffer[48];
+			memcpy(&packetBuffer, pstrRx->pu8Buffer, sizeof(packetBuffer));
+
+			if ((packetBuffer[0] & 0x7) != 4) {                   /* expect only server response */
+				printf("socket_cb: Expecting response from Server Only!\r\n");
+				return;                    /* MODE is not server, abort */
+			} else {
+				uint32_t secsSince1900 = packetBuffer[40] << 24 |
+						packetBuffer[41] << 16 |
+						packetBuffer[42] << 8 |
+						packetBuffer[43];
+
+				/* Now convert NTP time into everyday time.
+				 * Unix time starts on Jan 1 1970. In seconds, that's 2208988800.
+				 * Subtract seventy years.
+				 */
+				const uint32_t seventyYears = 2208988800UL;
+				uint32_t epoch = secsSince1900 - seventyYears;
+
+				/* Print the hour, minute and second.
+				 * GMT is the time at Greenwich Meridian.
+				 */
+				printf("socket_cb: The GMT time is %lu:%02lu:%02lu\r\n",
+						(epoch  % 86400L) / 3600,           /* hour (86400 equals secs per day) */
+						(epoch  % 3600) / 60,               /* minute (3600 equals secs per minute) */
+						epoch % 60);                        /* second */
+
+				ret = close(sock);
+				if (ret == SOCK_ERR_NO_ERROR) {
+					udp_socket = -1;
+				}
+			}
+		}
+	}
+	break;
+
+	default:
+		break;
+	}
 }
 
 int main(void)
